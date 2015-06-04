174029017da7dfa78ae15a0329e95a6282e02cac
John Potter <johnpotter1440@gmail.com>
2015-06-03 18:06:21 -0700

    initial commit

---
+++ lib/omniauth/feedly.rb
@@ -0,0 +1,7 @@
+require "omniauth/feedly/version"
+
+module Omniauth
+  module Feedly
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/feedly/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Feedly
+    VERSION = "0.0.1"
+  end
+end
